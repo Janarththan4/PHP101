b7337120517244c54872799847c4759369d3bb0d
Janarththan <ssjanarththan.4@gmail.com>
2023-07-23 10:20:16 +0530

    AssociativeArrays

--- index.php
+++ index.php
@@ -75,6 +75,16 @@
         ],
     ];
     ?>
+
+    <ul>
+        <?php foreach ($books as $book) : ?>
+            <li>
+                <a href="<?= $book['purchaseUrl'] ?>">
+                    <?= $book['name'] ?>
+                </a>
+            </li>
+        <?php endforeach; ?>
+    </ul>
 </body>
 
 </html>
